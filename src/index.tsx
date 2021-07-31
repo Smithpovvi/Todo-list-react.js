a1a59579c8c6f5589cbc853efad3b25eab05d8ae
Smithpovvi <vbairash@gmail.com>
2021-08-01 00:04:56 +0300

    add heshrout

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./main/ui/app/App";
 import { store } from "./main/bll/store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
