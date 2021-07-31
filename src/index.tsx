2da2e24aae88eb0ead98d38b91b5fd65b9e40584
Smithpovvi <vbairash@gmail.com>
2021-08-01 00:38:13 +0300

    fix bag 3

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./main/ui/app/App";
 import { store } from "./main/bll/store";
 import reportWebVitals from "./reportWebVitals";
